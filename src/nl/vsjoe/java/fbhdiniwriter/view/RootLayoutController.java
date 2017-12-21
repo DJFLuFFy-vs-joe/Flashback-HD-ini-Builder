adfc52af1b0a38f6e058a75ae670a72d6955ce29
FLuFFy <FLuFFy@192.168.1.141>
2017-12-21 01:16:23 +0100

    Created a RootLayout controller

---
+++ src/nl/vsjoe/java/fbhdiniwriter/view/RootLayoutController.java
@@ -0,0 +1,22 @@
+package nl.vsjoe.java.fbhdiniwriter.view;
+
+import nl.vsjoe.java.fbhdiniwriter.Main;
+
+/**
+ * The controller for the root layout. The root layout provides the basic
+ * application layout containing a menu bar and space where other JavaFX
+ * elements can be placed.
+ * 
+ * @author DJFLuFFy vs joe
+ */
+
+public class RootLayoutController {
+	
+	private Main main;
+	
+	public void setMain (Main main) {
+		this.main = main;
+	}
+	
+	
+}
