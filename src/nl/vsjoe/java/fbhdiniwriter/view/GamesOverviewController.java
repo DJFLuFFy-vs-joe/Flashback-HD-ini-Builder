d52cab45467542f0b2d5e68f18a451b46786f706
DJFLuFFy_vs_joe <djfluffy@vsjoe.nl>
2017-12-21 09:17:55 +0100

    Added a main var with a setter

--- src/nl/vsjoe/java/fbhdiniwriter/view/GamesOverviewController.java
+++ src/nl/vsjoe/java/fbhdiniwriter/view/GamesOverviewController.java
@@ -1,10 +1,18 @@
 package nl.vsjoe.java.fbhdiniwriter.view;
 
+import nl.vsjoe.java.fbhdiniwriter.Main;
+
 /**
  * 
  * @author DJFLuFFy vs joe
  *
  */
 public class GamesOverviewController {
+	
+	private Main main;
+	
+	public void setMain(Main main) {
+		this.main = main;
+	}
 
 }
