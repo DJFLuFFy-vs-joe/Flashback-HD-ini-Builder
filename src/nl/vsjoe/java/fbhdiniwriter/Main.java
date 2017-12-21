170dc9c8284c3aee601cc0b93c02a1f01758440b
DJFLuFFy_vs_joe <djfluffy@vsjoe.nl>
2017-12-21 09:21:18 +0100

    Added the renderGamesOverview method

--- src/nl/vsjoe/java/fbhdiniwriter/Main.java
+++ src/nl/vsjoe/java/fbhdiniwriter/Main.java
@@ -1,11 +1,13 @@
 package nl.vsjoe.java.fbhdiniwriter;
-	
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import nl.vsjoe.java.fbhdiniwriter.libs.Loc;
+import nl.vsjoe.java.fbhdiniwriter.view.GamesOverviewController;
 import nl.vsjoe.java.fbhdiniwriter.view.RootLayoutController;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
 /**
@@ -15,38 +17,58 @@ import javafx.scene.layout.BorderPane;
  */
 
 public class Main extends Application {
-	
+
 	private Stage primaryStage;
 	private BorderPane rootLayout;
-	
+
 	@Override
 	public void start(Stage primaryStage) {
 		this.primaryStage = primaryStage;
 		this.primaryStage.setTitle(Loc.TITLE);
-		
+
 		initRootLayout();
+
+		renderGamesOverview();
 	}
-	
-	
-	
+
+
+
+	public void renderGamesOverview() {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource(Loc.getLayout("GamesOverview")));
+			AnchorPane gamesOverview = loader.load();
+			
+			rootLayout.setCenter(gamesOverview);
+			
+			GamesOverviewController controller = loader.getController();
+			controller.setMain(this);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+
+
 	public void initRootLayout() {
 		try {
 			FXMLLoader loader = new FXMLLoader();
 			loader.setLocation(Main.class.getResource(Loc.getLayout("Rootlayout")));
 			rootLayout = loader.load();
-			
+
 			Scene scene = new Scene(rootLayout);
 			primaryStage.setScene(scene);
-			
+
 			RootLayoutController controller = loader.getController();
 			controller.setMain(this);
-			
+
 			primaryStage.show();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		//TODO create some Logic
-		
+
 	}
 
 
