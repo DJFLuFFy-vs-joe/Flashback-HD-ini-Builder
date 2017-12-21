42de7afc132e2d0b27e6287bc51e767f0aaccec8
DJFLuFFy_vs_joe <djfluffy@vsjoe.nl>
2017-12-21 10:36:57 +0100

    Added some simple vars to start testing the app

--- src/nl/vsjoe/java/fbhdiniwriter/view/GamesOverviewController.java
+++ src/nl/vsjoe/java/fbhdiniwriter/view/GamesOverviewController.java
@@ -1,6 +1,10 @@
 package nl.vsjoe.java.fbhdiniwriter.view;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import nl.vsjoe.java.fbhdiniwriter.Main;
+import nl.vsjoe.java.fbhdiniwriter.model.Game;
 
 /**
  * 
@@ -8,8 +12,14 @@ import nl.vsjoe.java.fbhdiniwriter.Main;
  *
  */
 public class GamesOverviewController {
-	
+
 	private Main main;
+	
+	@FXML
+	private TableView<Game> gameTable;
+	
+	@FXML
+	private TableColumn<Game, String> gameName;
 	
 	public void setMain(Main main) {
 		this.main = main;
