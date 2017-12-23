a2cb13982e7b88b223396ab3f90b7f49b41721dd
DJFLuFFy_vs_joe <dj.fluffy@gmail.com>
2017-12-23 17:36:06 +0100

    added a version var

--- src/nl/vsjoe/java/fbhdiniwriter/libs/Loc.java
+++ src/nl/vsjoe/java/fbhdiniwriter/libs/Loc.java
@@ -7,6 +7,8 @@ package nl.vsjoe.java.fbhdiniwriter.libs;
  */
 
 public class Loc {
+	public static final String VERSION = "1.0";
+	
 	public static final String VIEW = "view/";
 	public static final String TITLE = "FlashBack HD ini Builder";
 	
@@ -24,6 +26,7 @@ public class Loc {
 	public static final String GAMES_FOLDER = "games";
 	public static final String INI_FOLDER = "ini_folder";
 	public static final String INI_FILE = INI_FOLDER + "/all-games.ini";
+	
 	
 	
 }
