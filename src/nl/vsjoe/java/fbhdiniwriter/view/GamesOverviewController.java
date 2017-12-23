bfa6ae562d390518365d09be6055019019ac77ac
DJFLuFFy_vs_joe <dj.fluffy@gmail.com>
2017-12-23 14:55:38 +0100

    Added labels and text for the Extra Game info

--- src/nl/vsjoe/java/fbhdiniwriter/view/GamesOverviewController.java
+++ src/nl/vsjoe/java/fbhdiniwriter/view/GamesOverviewController.java
@@ -1,6 +1,8 @@
 package nl.vsjoe.java.fbhdiniwriter.view;
 
+import javafx.beans.Observable;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import nl.vsjoe.java.fbhdiniwriter.Main;
@@ -22,9 +24,67 @@ public class GamesOverviewController {
 	private TableColumn<Game, String> gameName;
 	
 	@FXML
+	private TextField gameNameTextField;
+	
+	@FXML
+	private TextField systemTextField;
+	
+	@FXML
+	private TextField genreTextField;
+	
+	@FXML
+	private TextField descriptionTextField;
+	
+	@FXML
+	private TextField dPadTextField;
+	
+	@FXML
+	private TextField startTextField;
+	
+	@FXML
+	private TextField buttonATextField;
+	
+	@FXML
+	private TextField buttonBTextField;
+	
+	@FXML
+	private TextField buttonCTextField;
+	
+	@FXML
+	private TextField buttonXTextField;
+	
+	@FXML
+	private TextField buttonYTextField;
+	
+	@FXML
+	private TextField buttonZTextField;
+	
+	@FXML
 	private void initialize() {
 		gameName.setCellValueFactory(cellData -> cellData.getValue().getGameNameProperty());
 		
+		showGameDetails(null);
+		
+		gameTable.getSelectionModel().selectedItemProperty().addListener(
+				(Observable, oldValue, newValue) -> showGameDetails(newValue));
+		
+	}
+	
+	private void showGameDetails(Game game) {
+		if (game != null) {
+			gameNameTextField.setText(game.getGameName());
+			systemTextField.setText(game.getPlatform());
+			genreTextField.setText(game.getGenre());
+			descriptionTextField.setText(game.getDescription());
+			dPadTextField.setText(game.getPadD());
+			startTextField.setText(game.getStart());
+			buttonATextField.setText(game.getBtnA());
+			buttonBTextField.setText(game.getBtnB());
+			buttonCTextField.setText(game.getBtnC());
+			buttonXTextField.setText(game.getBtnX());
+			buttonYTextField.setText(game.getBtnY());
+			buttonZTextField.setText(game.getBtnZ());
+		}
 	}
 	
 	public void setMain(Main main) {
